e6e44a79e42e52f9cb7fcc93cd8064c4e801e781
Samdevops789 <Sampathbandla2@gmail.com>
2023-01-17 16:57:17 +0530

    updating ./learning-devops7/user-infra

---
+++ immutable/env/dev.bacckend.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-b66"
+key    = "immutable/user-infra/dev/terraform.tfstate"
+region = "us-east-1"
---
+++ mutable/main.tf
@@ -0,0 +1,11 @@
+module "user" {
+  source           = "./vendor/modules/app"
+  COMPONENT        = var.COMPONENT
+  ENV              = var.ENV
+  INSTANCES        = var.INSTANCES
+  APP_PORT         = 8080
+  LB_RULE_PRIORITY = 1
+}
+
+
+
